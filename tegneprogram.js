8bacf73f31e686d8466f93abea625456e01ce2b3
Bendik <playstationbendik@gmail.com>
2025-01-31 11:32:39 +0100

    a

--- tegneprogram.js
+++ tegneprogram.js
@@ -4,10 +4,14 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 canvas.width = width
 canvas.height = height
+const tegnebrett = document.querySelector('.tegnebrett')
 
 let xMouse = 0
 let yMouse = 0
 let rect = canvas.getBoundingClientRect();
+
+tegnebrett.style.gridTemplateColumns = "100px width 100px"
+tegnebrett.style.gridTemplateRows = "100px height 100px"
 
 function rektangel5(x, y) {
     ctx.beginPath()
